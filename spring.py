401c8ea2707c4bafad616f761747e343d26abdc2
yemreyalcin2707 <115788239+yemreyalcin2707@users.noreply.github.com>
2023-01-18 19:18:24 +0300

    Update spring.py

--- spring.py
+++ spring.py
@@ -12,6 +12,7 @@ class Player(pygame.sprite.Sprite):
 		self.yay.append(pygame.image.load('-0.5.png'))
 		self.yay.append(pygame.image.load('0.png'))
 		self.yay.append(pygame.image.load('0.5.png'))
+		self.yay.append(pygame.image.load('1.png'))
 	
 run = True
 while run:
